1b208ec52c8c21a60190f0fcf66da6fe14b5b4d2
Everest Munro-Zeisberger <evmunro1@gmail.com>
2020-11-30 20:48:09 -0800

    Comment out fuzz function with legitimate type error

--- pkg/sql/pgwire/fuzz.go
+++ pkg/sql/pgwire/fuzz.go
@@ -12,47 +12,33 @@
 
 package pgwire
 
-import (
-	"context"
-	"io"
-	"io/ioutil"
-	"net"
-	"time"
+// func FuzzServeConn(data []byte) int {
+// 	s := MakeServer(
+// 		log.AmbientContext{},
+// 		&base.Config{},
+// 		&cluster.Settings{},
+// 		sql.MemoryMetrics{},
+// 		&mon.BytesMonitor{},
+// 		time.Minute,
+// 		&sql.ExecutorConfig{
+// 			Settings: &cluster.Settings{},
+// 		},
+// 	)
 
-	"github.com/cockroachdb/cockroach/pkg/base"
-	"github.com/cockroachdb/cockroach/pkg/settings/cluster"
-	"github.com/cockroachdb/cockroach/pkg/sql"
-	"github.com/cockroachdb/cockroach/pkg/util/log"
-	"github.com/cockroachdb/cockroach/pkg/util/mon"
-)
-
-func FuzzServeConn(data []byte) int {
-	s := MakeServer(
-		log.AmbientContext{},
-		&base.Config{},
-		&cluster.Settings{},
-		sql.MemoryMetrics{},
-		&mon.BytesMonitor{},
-		time.Minute,
-		&sql.ExecutorConfig{
-			Settings: &cluster.Settings{},
-		},
-	)
-
-	// Fake a connection using a pipe.
-	srv, client := net.Pipe()
-	go func() {
-		// Write the fuzz data to the connection and close.
-		_, _ = client.Write(data)
-		_ = client.Close()
-	}()
-	go func() {
-		// Discard all data sent from the server.
-		_, _ = io.Copy(ioutil.Discard, client)
-	}()
-	err := s.ServeConn(context.Background(), srv)
-	if err != nil {
-		return 0
-	}
-	return 1
-}
+// 	// Fake a connection using a pipe.
+// 	srv, client := net.Pipe()
+// 	go func() {
+// 		// Write the fuzz data to the connection and close.
+// 		_, _ = client.Write(data)
+// 		_ = client.Close()
+// 	}()
+// 	go func() {
+// 		// Discard all data sent from the server.
+// 		_, _ = io.Copy(ioutil.Discard, client)
+// 	}()
+// 	err := s.ServeConn(context.Background(), srv)
+// 	if err != nil {
+// 		return 0
+// 	}
+// 	return 1
+// }
